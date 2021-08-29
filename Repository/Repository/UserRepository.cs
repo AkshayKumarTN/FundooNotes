390ba133a8c4b5fe88665be4816025ab3e8b62a6
Akshay Kumar T N <akshay.t@tvsnext.io>
2021-08-29 22:52:07 +0530

    [Akshay] Added : Method to Encrypt Password

--- Repository/Repository/UserRepository.cs
+++ Repository/Repository/UserRepository.cs
@@ -22,6 +22,7 @@ namespace FundooNotes.Repository.Repository
             {
                 if(userData !=null)
                 {
+                    userData.Password = EncodePasswordToBase64(userData.Password);
                     this.userContext.Users.Add(userData);
                     this.userContext.SaveChanges();
                     return true;
@@ -33,6 +34,22 @@ namespace FundooNotes.Repository.Repository
                 throw new Exception(ex.Message);
             }
             
+        }
+
+        // this function Convert to Encord your Password....
+        public static string EncodePasswordToBase64(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
         }
 
     }
